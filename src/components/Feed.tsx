1d12d32a8391862aee52f0347faabd60e272d219
Aldiyar Kabulov <reservecrate@gmail.com>
2021-11-30 14:22:45 +0600

    added json-server

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -23,7 +23,7 @@ const Feed = () => {
         }
     ]);
     useEffect(() => {
-        fetch('http://localhost:3001')
+        fetch('http://localhost:5000/posts')
             .then(res => res.json())
             .then(postsData => setPosts(postsData));
     }, []);
---
+++ src/components/Subreddits.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Stack } from '@mui/material';
+
+const Subreddits = () => {
+    return <Stack></Stack>;
+};
+
+export default Subreddits;
